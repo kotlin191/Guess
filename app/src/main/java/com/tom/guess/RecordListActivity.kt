3c29629cb36601b698298c871b2bbfd188539acb
Hank Tom <hanktom@gmail.com>
2019-09-05 17:22:51 +0800

    p27 coroutines activity lifecycle

--- app/src/main/java/com/tom/guess/RecordListActivity.kt
+++ app/src/main/java/com/tom/guess/RecordListActivity.kt
@@ -6,22 +6,34 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.tom.guess.data.GameDatabase
 import kotlinx.android.synthetic.main.activity_record_list.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
-class RecordListActivity : AppCompatActivity() {
+class RecordListActivity : AppCompatActivity(), CoroutineScope {
+    private lateinit var job: Job
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_record_list)
+        job = Job()
         //get records
-        AsyncTask.execute {
-            val records = GameDatabase.getInstance(this)?.recordDao()?.getAll()
+        launch {
+            val records = GameDatabase.getInstance(this@RecordListActivity)?.recordDao()?.getAll()
             records?.let {
-                runOnUiThread {
-                    recycler.layoutManager = LinearLayoutManager(this)
-                    recycler.setHasFixedSize(true)
-                    recycler.adapter = RecordAdapter(it)
-                }
+                recycler.layoutManager = LinearLayoutManager(this@RecordListActivity)
+                recycler.setHasFixedSize(true)
+                recycler.adapter = RecordAdapter(it)
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
     }
 }
